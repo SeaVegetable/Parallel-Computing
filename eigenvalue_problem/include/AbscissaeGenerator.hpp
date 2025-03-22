c1915cd588af6c86981531896175c82aad0d3a6d
SeaVegetable <1135167789@qq.com>
2025-03-22 14:49:47 +0800

    add abscissae

---
+++ eigenvalue_problem/include/AbscissaeGenerator.hpp
@@ -0,0 +1,19 @@
+#ifndef ABSCISSAE_GENERATOR_HPP
+#define ABSCISSAE_GENERATOR_HPP
+
+class AbscissaeGenerator
+{
+    public:
+        std::vector<double> GenerateAbscissae1D(
+            const std::vector<double> &S, const int &p, const int &idx);
+
+        std::vector<double> GenerateAbscissae2D(const std::vector<double> &S,
+            const std::vector<double> &T, const int &p, const int &q);
+        
+        std::vector<double> GrevilleAbscissae(
+            const std::vector<double> &S, const int &p);
+
+        // std::vector<double> DemkoAbscissae(
+        //     const std::vector<double> &S, const int &p);
+};
+#endif
