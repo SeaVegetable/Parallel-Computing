5f2f5110ee64d02a1ccfd3cb1afe36ac83746b67
SeaVegetable <1135167789@qq.com>
2025-07-30 11:03:37 +0800

    finish element routine for fem part

--- matrix_free_gpu/src/GlobalAssembly.cpp
+++ matrix_free_gpu/src/GlobalAssembly.cpp
@@ -1,6 +1,6 @@
 #include "GlobalAssembly.hpp"
 
-__global__ void AssembleKernel(const int nLocBas,
+__global__ void AssembleKernel(const int nLocBas, const int nqp,
     const double * d_N, const double * d_dN_dxi, const double * d_dN_deta,
     const double * d_weight,
     const int * d_IEN, const int * d_ID, const int * d_Dir,
@@ -24,15 +24,66 @@ __global__ void AssembleKernel(const int nLocBas,
 
     __syncthreads();
 
+    int qp = threadIdx.y * blockDim.x + threadIdx.x;
 
+    double R[nLocBas];
+    double dN_dxi_q[nLocBas];
+    double dN_deta_q[nLocBas];
+
+    if (qp < nqp)
+    {
+        for (int i = 0; i < nLocBas; ++i)
+        {
+            R[i] = d_N[qp * nLocBas + i];
+            dN_dxi_q[i] = d_dN_dxi[qp * nLocBas + i];
+            dN_deta_q[i] = d_dN_deta[qp * nLocBas + i];
+        }
+    }
+
+    __syncthreads();
+
+    double jacobian;
+    double dR_dx[nLocBas];
+    double dR_dy[nLocBas];
+    compute_jacobian_basis_derivative(nLocBas, dN_dxi_q, dN_deta_q, s_eCP, jacobian, dR_dx, dR_dy);
 }
 
-__device__ void ElementKernel(const int nLocBas,
-    const double * d_N, const double * d_dN_dxi, const double * d_dN_deta,
-    const double * d_weight,
-    const double * s_eCP)
+__device__ void compute_jacobian_basis_derivative(
+    const int nLocBas, const double * dN_dxi, const double * dN_deta,
+    const double * eCP, double &jacobian, double * dR_dx, double * dR_dy)
 {
-    
+    double dx_dxi = 0.0;
+    double dx_deta = 0.0;
+    double dy_dxi = 0.0;
+    double dy_deta = 0.0;
+
+    jacobian = 0.0;
+
+    double dxi_dx = 0.0;
+    double dxi_dy = 0.0;
+    double deta_dx = 0.0;
+    double deta_dy = 0.0;
+
+    for (int ii = 0; ii < nLocBas; ++ii)
+    {
+        dx_dxi += eCP[ii * 2] * dN_dxi[ii];
+        dx_deta += eCP[ii * 2] * dN_deta[ii];
+        dy_dxi += eCP[ii * 2 + 1] * dN_dxi[ii];
+        dy_deta += eCP[ii * 2 + 1] * dN_deta[ii];
+    }
+
+    jacobian = dx_dxi*dy_deta - dx_deta*dy_dxi;
+
+    dxi_dx = dy_deta/jacobian;
+    dxi_dy = -dy_dxi/jacobian;
+    deta_dx = -dx_deta/jacobian;
+    deta_dy = dx_dxi/jacobian;
+
+    for (int ii = 0; ii < nLocBas; ++ii)
+    {
+        dR_dx[ii] = dxi_dx * dN_dxi[ii] + dxi_dy * dN_deta[ii];
+        dR_dy[ii] = deta_dx * dN_dxi[ii] + deta_dy * dN_deta[ii];
+    }
 }
 
 GlobalAssembly::GlobalAssembly(const int &nlocalfunc,
