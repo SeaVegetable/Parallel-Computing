ba99fb415cd5c687bb1ec2bffc425bd9d325735b
SeaVegetable <1135167789@qq.com>
2025-08-12 17:44:44 +0800

    minor

--- test_petsc_with_gpu/include/memory.cuh
+++ test_petsc_with_gpu/include/memory.cuh
@@ -1,9 +1,34 @@
 #pragma once
-template <class T>
-void MallocDeviceMemory(T **ptr, size_t n);
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include "memory.hpp"
 
 template <class T>
-void FreeDeviceMemory(T *ptr);
+void MallocDeviceMemory(T **ptr, size_t n)
+{
+    cudaError_t err = cudaMalloc((void**)ptr, n * sizeof(T));
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error allocating device memory: %s\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+}
 
 template <class T>
-void CopyToDevice(T *dst, const T *src, size_t n);
+void FreeDeviceMemory(T *ptr)
+{
+    cudaError_t err = cudaFree(ptr);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error freeing device memory: %s\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+}
+
+template <class T>
+void CopyToDevice(T *dst, const T *src, size_t n)
+{
+    cudaError_t err = cudaMemcpy(dst, src, n * sizeof(T), cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Error copying to device memory: %s\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+}
