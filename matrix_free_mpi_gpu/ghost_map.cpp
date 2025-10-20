70bf9f4290e84da7058b4cc1b2bf8face1e3b70b
SeaVegetable <1135167789@qq.com>
2025-10-20 16:48:21 +0800

    ghost map

--- matrix_free_mpi_gpu/ghost_map.cpp
+++ matrix_free_mpi_gpu/ghost_map.cpp
@@ -19,6 +19,14 @@ int main(int argc, char **argv)
     const int total_ranks = part_num_1d * part_num_1d;
     for (int rank = 0; rank < total_ranks; ++rank)
     {
+        int roffset = 0;
+        int soffset = 0;
+        std::vector<int> recieveIndices;
+        std::vector<int> recieveRanks;
+        std::vector<int> recieveOffsets;
+        std::vector<int> sendIndices;
+        std::vector<int> sendRanks;
+        std::vector<int> sendOffsets;
         std::string filename = fm->GetPartitionFilename(base_name, rank);
         fm->ReadPartition(filename, ownlocalID, ownghostID);
         for (int other_rank = 0; other_rank < total_ranks; ++other_rank)
@@ -27,7 +35,31 @@ int main(int argc, char **argv)
             {
                 std::string other_filename = fm->GetPartitionFilename(base_name, other_rank);
                 fm->ReadPartition(other_filename, otherlocalID, otherghostID);
+                for (const auto &id : ownlocalID)
+                {
+                    if (std::find(otherghostID.begin(), otherghostID.end(), id) != otherghostID.end())
+                    {
+                        sendIndices.push_back(id);
+                        sendRanks.push_back(other_rank);
+                        soffset++;
+                    }
+                }
+                for (const auto &id : ownghostID)
+                {
+                    if (std::find(otherlocalID.begin(), otherlocalID.end(), id) != otherlocalID.end())
+                    {
+                        recieveIndices.push_back(id);
+                        recieveRanks.push_back(other_rank);
+                        roffset++;
+                    }
+                }
+                recieveOffsets.push_back(roffset);
+                sendOffsets.push_back(soffset);
             }
         }
+        recieveRanks.erase(std::unique(recieveRanks.begin(), recieveRanks.end()), recieveRanks.end());
+        recieveOffsets.erase(std::unique(recieveOffsets.begin(), recieveOffsets.end()), recieveOffsets.end());
+        sendRanks.erase(std::unique(sendRanks.begin(), sendRanks.end()), sendRanks.end());
+        sendOffsets.erase(std::unique(sendOffsets.begin(), sendOffsets.end()), sendOffsets.end());
     }
 }
